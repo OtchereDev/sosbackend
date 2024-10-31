0f756aeeee2e9e71e913efedb9f537702126d667
Oliver Otchere <oliverotchere4@gmail.com>
2024-10-31 15:37:30 +0000

    added guide modules

--- src/guide/guide.controller.ts
+++ src/guide/guide.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { GuideService } from './guide.service';
+import { CreateCategory } from './dto/CreateCategory.dto';
 
 @Controller('guide')
 export class GuideController {
   constructor(private service: GuideService) {}
+
+  @Get('/')
+  async getAllGuides() {
+    return await this.service.getAllGuides();
+  }
+
+  @Get('/:id')
+  async getGuide(@Param('id') id: string) {
+    return await this.service.getGuide(id);
+  }
+
+  @Get('/categories')
+  async getAllCategories() {
+    return await this.service.getAllCategory();
+  }
+
+  @Post('/categories')
+  async createCategory(@Body() body: CreateCategory) {
+    return await this.service.createCategory(body.name);
+  }
+
+  @Post('/')
+  async createGuide() {}
 }
--- src/guide/guide.service.ts
+++ src/guide/guide.service.ts
@@ -1,4 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { EmergencyGuide } from './models/EmergencyGuide.models';
+import { Category } from './models/Category.models';
 
 @Injectable()
-export class GuideService {}
+export class GuideService {
+  constructor(
+    @InjectModel(EmergencyGuide.name) private guideModel: Model<EmergencyGuide>,
+    @InjectModel(Category.name) private categoryModel: Model<Category>,
+  ) {}
+
+  async getAllGuides() {
+    const guides = await this.guideModel.find({}, undefined, {
+      populate: ['category'],
+    });
+
+    return {
+      status: 200,
+      data: {
+        guides,
+      },
+    };
+  }
+
+  async getGuide(id: string) {
+    const guide = await this.guideModel.findById(id, undefined, {
+      populate: ['category'],
+    });
+
+    return {
+      status: 200,
+      data: {
+        guide,
+      },
+    };
+  }
+
+  async getAllCategory() {
+    const categories = await this.categoryModel.find({});
+
+    return {
+      status: 200,
+      data: {
+        categories,
+      },
+    };
+  }
+
+  async createCategory(name: string) {
+    await this.categoryModel.create({
+      name,
+    });
+
+    return {
+      status: 200,
+      message: 'Category successfully created',
+    };
+  }
+}
